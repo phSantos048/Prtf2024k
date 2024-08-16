8046b444596310b76048e54352d8cf28e9bd8314
phSantos048 <128161966+phSantos048@users.noreply.github.com>
2024-08-16 14:31:30 -0300

    Add files via upload

---
+++ Áreas/trimestres.js
@@ -0,0 +1,19 @@
+
+function voltarParaInicio2() {
+    location.href = '../index.html';
+}
+function irParaHumanas2() {
+    location.href = '../matérias trimestre 2/Ciências Humanas.html';
+}
+function irParaLinguagens2() {
+    location.href = '../matérias trimestre 2/linguagens.html';
+}
+function irParaMatematica2() {
+    location.href = '../matérias trimestre 2/Matemática.html';
+}
+function irParaNatureza2() {
+    location.href = '../matérias trimestre 2/Ciências da Natureza.html';
+}
+function irParaTecnico2() {
+    location.href = '../matérias trimestre 2/Técnico.html';
+}
